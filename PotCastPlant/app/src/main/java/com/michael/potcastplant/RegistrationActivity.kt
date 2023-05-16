dc323040e76eaa9fa12879bc6d572211475d1529
Michael <michael.ehiosumwen@student.nhlstenden.com>
2023-05-16 16:23:22 +0200

    I created the structure of the app as well as the splash screen & login page

---
+++ PotCastPlant/app/src/main/java/com/michael/potcastplant/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.michael.potcastplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
