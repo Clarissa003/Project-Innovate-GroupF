3ed5aa46ced3557898e61202332527d09eb2c87a
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2023-05-29 18:55:30 +0200

    First try at the notification page

---
+++ PotCastPlant/app/src/main/java/com/michael/potcastplant/NotificationAdapter.kt
@@ -0,0 +1,30 @@
+package com.michael.potcastplant
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotificationAdapter(private var notification: Array<NotificationClass>) : RecyclerView.Adapter<NotificationAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return notification.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_notification, parent, false)
+        return NotificationAdapter.ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotificationAdapter.ViewHolder, position: Int) {
+        val NotificationClass = notification[position]
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val cardView: CardView = itemView.findViewById(R.id.cvNotifications)
+    }
+}
