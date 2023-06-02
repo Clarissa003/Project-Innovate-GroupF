482d2bbfe53fd1399a9330f6d4619640f209f31f
Michael <michael.ehiosumwen@student.nhlstenden.com>
2023-06-02 11:13:42 +0200

    I completed the backend for the registration and login page

--- PotCastPlant/app/src/main/java/com/michael/potcastplant/LoginActivity.kt
+++ PotCastPlant/app/src/main/java/com/michael/potcastplant/LoginActivity.kt
@@ -3,22 +3,30 @@ package com.michael.potcastplant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.michael.potcastplant.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         setOnClickListeners()
     }
 
     private fun setOnClickListeners() {
         binding.buttonLogin.setOnClickListener {
-            val intent = Intent(this, NavigationHostActivity::class.java)
-            startActivity(intent)
+            performLogin()
         }
 
         binding.textViewForgotPassword.setOnClickListener {
@@ -30,6 +38,32 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performLogin() {
+
+        binding.buttonLogin.isEnabled = false
+        val email = binding.editTextEmail.text.toString()
+        val password = binding.editTextPassword.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Fields cannot be Empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, NavigationHostActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                    binding.buttonLogin.isEnabled = true
+                }
+            }
+
+        binding.buttonLogin.isEnabled = true
     }
 
 }
