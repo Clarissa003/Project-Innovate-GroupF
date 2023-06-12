0fce3d47e460ac6919cc198c3acde617c246cf70
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2023-06-12 15:00:03 +0200

    fixed profile backend

--- PotCastPlant/app/src/main/java/com/michael/potcastplant/ProfileActivity.kt
+++ PotCastPlant/app/src/main/java/com/michael/potcastplant/ProfileActivity.kt
@@ -8,12 +8,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.michael.potcastplant.databinding.ActivityProfileBinding
 
 
 class ProfileActivity : Fragment() {
 
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var firestore : FirebaseFirestore
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,24 +27,23 @@ class ProfileActivity : Fragment() {
         binding = ActivityProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
-    
-    var sharedPreferences: SharedPreferences = getSharedPreferences("MyDatabase", Context.MODE_PRIVATE)
-
-    var nameData = sharedPreferences.getString("key", "Your name")
-    val name = binding.nameText.text.toString()
-    name.setText(nameData)
-
-    var emailData = sharedPreferences.getString("key", "Your email")
-    val email = binding.emailText.text.toString()
-    name.setText(emailData)
-
-    val picture = binding.profileImage
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        var sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("MyPref",Context.MODE_PRIVATE)
+        val uid = sharedPreferences.getString("uid", null) ?: ""
+
+        val name = binding.nameText.text.toString()
+        val profilePic = binding.profileImage
+        val email = binding.emailText.text.toString()
+
+
         binding.editProfileButton.setOnClickListener {
-            // Edit profile button click
+            // Edit profile button after click
 
         }
 
