7ce679bbde20f4bb10d66469752c6bee2c7bc638
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2023-06-22 15:40:42 +0200

    Motor arduino with the library

--- sensors_motors/pump/arduino/no_library/sketch_jun22a/sketch_jun22a.ino
+++ sensors_motors/pump/arduino/no_library/sketch_jun22a/sketch_jun22a.ino
@@ -1,13 +1,12 @@
+
 const int motorPin = 9;
 void setup() {
   pinMode(motorPin, OUTPUT);
 }
-
 void loop() {
-  // Turn the motor on
   digitalWrite(motorPin, HIGH);
-  delay(1000); // Wait for 1 second
+  delay(1000); 
   // Turn the motor off
   digitalWrite(motorPin, LOW);
-  delay(1000);
+  delay(1000); // Wait for 1 second
 }
