63042140a52771c4400b5dc70d4814bc62db70f4
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2023-06-23 18:25:34 +0200

    Updating the API update to function for sunlight and looping

--- api_connection/ApiUppdate.py
+++ api_connection/ApiUppdate.py
@@ -2,17 +2,18 @@
 import network
 import urequests
 import json
+import utime
 
-humidity = 0
-sunlight = 0
-moisture = 0
-temperature = 0
-water_level = 0
+humidity = 1
+sunlight = 8
+moisture = 3
+temperature = 4
+water_level = 5
 
 # wifi_ssid = '0000'
 # wifi_password = '0000'
 
-# wifi_ssid = 'samba1'
+# wifi_ssid = 'SAMBA'
 # wifi_password = ''
 
 # wifi_ssid = 'Ziggo-ap-4d4be42'
@@ -29,6 +30,32 @@ def connect_to_wifi():
     while not wlan.isconnected():
         pass
     print('Connected to WLAN')
+    
+def receive_send(value):
+    # 1 For toggling automatic pump on
+    # 2 For toggling automatic pump off
+    # 3 For getting soil moisture
+    # 4 For getting temp
+    # 5 For humidity
+    # 6 For water level
+    # 7 For Light
+    uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1))  # Adjust the UART pins as per your connections
+    uart.write(str(value))
+    while True:
+        if uart.any():
+            data = uart.read()
+            if value == 3:
+                globals().update({"soil_moisture": data.decode()})
+            elif value == 4:
+                globals().update({"temperature": data.decode()})
+            elif value == 5:
+                globals().update({"humidity": data.decode()})
+            elif value == 6:
+                globals().update({"water_level": data.decode()})
+            elif value == 7:
+                globals().update({"sunlight": data.decode()})
+            print("Received:", data.decode())
+            break
 
 def api_send_data():
     data = {
@@ -50,8 +77,34 @@ def api_send_data():
                              headers=headers,
                              data=data_bytes)
     print(response.text)
+    print('Successfully sent data to endpoint')
     
-connect_to_wifi()
-api_send_data()
+def api_send_sunlight():
+    data = {
+        'sunlight' : int(sunlight)
+    }
+    json_data = json.dumps(data)  # Convert data to JSON
+    data_bytes = json_data.encode('utf-8')
 
+    content_length = len(data_bytes)  # Calculate content length
+    headers = {
+        'Content-Type': 'application/json',
+        'Content-Length': str(content_length)
+    }
 
+    response = urequests.put('https://us-central1-potcast-plant.cloudfunctions.net/app/api/pots/update/sunlight/1',
+                             headers=headers,
+                             data=data_bytes)
+    print(response.text)
+    print('Successfully sent data to endpoint')
+
+while True:
+    connect_to_wifi()
+    receive_send(3) # soil moisture
+    receive_send(4) # temperature 
+    receive_send(5) # humidity
+    receive_send(6) # water level
+    receive_send(7)
+    api_send_data()
+    api_send_sunlight()
+    usleep(120)
--- api_connection/apiputtest.py
+++ api_connection/apiputtest.py
@@ -1,82 +1,78 @@
-# Import modules
 import network
 import urequests
 import json
+import machine
+import utime
 
 humidity = 0
+sunlight = 0
 moisture = 0
-sunlight = 69
 temperature = 0
 water_level = 0
-
-# wifi_ssid = 'samba1'
-# wifi_password = ''
-
-# wifi_ssid = 'Ziggo-ap-4d4be42'
-# wifi_password = 'wwfCJv8xnyvz'
 
 wifi_ssid = 'asdf'
 wifi_password = 'awwm715#'
 
+def connect_to_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(wifi_ssid, wifi_password)
+    while not wlan.isconnected():
+        pass
+    print('Connected to WLAN')
 
-# Connect to WLAN
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(wifi_ssid, wifi_password)
-while not wlan.isconnected():
-    pass
-print('Connected to WLAN')
-
-def handle_put_request(url, data):
-    json_data = json.dumps(data)
-    data_bytes = json_data.encode('utf-8')
-    headers = {
-        'Content-Type': 'application/json',
-        'Content-Length': str(len(data_bytes))
-    }
-    response = urequests.put(url, headers=headers, data=data_bytes)  # Disable SSL/TLS encryption
-    print("Response Status Code:", response.status_code)
-    print("Response Headers:", response.headers)
-    print("Response Content:", response.content)
-    if response.status_code == 200:
-        if response.content:
-            return response.json()
-        else:
-            print("Error: Empty response content")
-    else:
-        print("Error: API request failed")
-    return None
-
-
+def receive_send(value):
+    uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1))  # Adjust the UART pins as per your connections
+    uart.write(str(value))
+    while True:
+        if uart.any():
+            data = uart.read()
+            if value == 3:
+                globals().update({"soil_moisture": data.decode()})
+            elif value == 4:
+                globals().update({"temperature": data.decode()})
+            elif value == 5:
+                globals().update({"humidity": data.decode()})
+            elif value == 6:
+                globals().update({"water_level": data.decode()})
+            elif value == 7:
+                globals().update({"sunlight": data.decode()})
+            print("Received:", data.decode())
+            break
 
 def api_send_data():
-    url = 'https://us-central1-potcast-plant.cloudfunctions.net/app/api/pots/update/1'
     data = {
-        'temperature': temperature,
-        'moisture': moisture,
-        'waterlevel': water_level,  # Set water level as needed
-        'humidity': humidity,
+        'temperature': int(temperature),
+        'moisture': int(moisture),
+        'waterlevel': int(water_level),
+        'humidity': int(humidity),
     }
-    response = handle_put_request(url, data)
-    if response is not None and response.status_code == 200:
-        print("Data sent successfully.")
-    else:
-        print("Error: Failed to send data.")
+    json_data = json.dumps(data)  # Convert data to JSON
+    data_bytes = json_data.encode('utf-8')
 
-def api_send_sunlight():
-    url = 'https://us-central1-potcast-plant.cloudfunctions.net/app/api/pots/update/sunlight/1'
-    data = {
-        'sunlight': sunlight,  # Use the actual sunlight variable value
+    content_length = len(data_bytes)  # Calculate content length
+    headers = {
+        'Content-Type': 'application/json',
+        'Content-Length': str(content_length)
     }
-    response = handle_put_request(url, data)
-    if response is not None and response.status_code == 200:
-        print("Sunlight data sent successfully.")
-    else:
-        print("Error: Failed to send sunlight data.")
 
-def main():
+    response = urequests.put('https://us-central1-potcast-plant.cloudfunctions.net/app/api/pots/update/1',
+                             headers=headers,
+                             data=data_bytes)
+    print(response.text)
+    print('Successfully sent data to endpoint')
+
+while True:
+    connect_to_wifi()
+
+    # Retrieve sensor data using receive_send() function
+    receive_send(3)  # soil moisture
+    receive_send(4)  # temperature
+    receive_send(5)  # humidity
+    receive_send(6)  # water level
+    receive_send(7)  # sunlight
+
+    # Send the data to the API
     api_send_data()
-    api_send_sunlight()
-
-if __name__ == "__main__":
-    main()
+    
+    utime.sleep(120)
