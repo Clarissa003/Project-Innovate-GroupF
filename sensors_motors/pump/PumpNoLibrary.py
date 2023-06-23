8b2ae59722f713c1fbbcd7cb7bc732103665ef21
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2023-06-23 15:26:27 +0200

    library screen micropython + changes

--- sensors_motors/pump/PumpNoLibrary.py
+++ sensors_motors/pump/PumpNoLibrary.py
@@ -1,12 +1,61 @@
-import machine
-import utime
+import socket
+import ssl
+import ujson
+wifi_ssid = 'samba1'
+wifi_password = ''
+def connect_to_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(wifi_ssid, wifi_password)
+    while not wlan.isconnected():
+        pass
+    print('Connected to WLAN')
+def api_get_boundary_parameters():
+    url = 'https://us-central1-potcast-plant.cloudfunctions.net/app/api/update/1'
+    port = 443
+    
+    # Establish a TCP socket connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(socket.getaddrinfo(url, port)[0][-1])
+    
+    # Wrap the socket with SSL/TLS
+    context = ssl.create_default_context()
+    secure_sock = context.wrap_socket(sock, server_hostname=url)
+    
+    # Send an HTTP GET request
+    request = "GET /api/plants/read/1 HTTP/1.0\r\nHost: {}\r\n\r\n".format(url)
+    secure_sock.write(request.encode())
+    
+    # Receive and process the response
+    response = secure_sock.read()
+    response_lines = response.split(b'\r\n')
+    
+    if response_lines[0].startswith(b'HTTP/1.1 200'):
+        response_body = b''.join(response_lines[response_lines.index(b'') + 1:])
+        data = ujson.loads(response_body)
+        globals().update({
+            "plant_id": data['id'],
+            "humidity_max": data['humidityMax'],
+            "humidity_min": data['humidityMin'],
+            "sunlight_max": data['sunlightMax'],
+            "sunlight_min": data['sunlightMin'],
+            "moisture_max": data['moistureMax'],
+            "moisture_min": data['moistureMin'],
+            "temperature_max": data['temperatureMax'],
+            "temperature_min": data['temperatureMin']
+        })
+    else:
+        print("Error: API request failed")
+    
+    # Close the secure socket
+    secure_sock.close()
 
-# Configure GPIO pin
-motor_pin = machine.Pin(16, machine.Pin.OUT)
-
-# Turn the motor on and off in a loop
+# Call the API function
+api_get_boundary_parameters()
 while True:
-    motor_pin.on()  # Turn the motor on
-    utime.sleep(1)  # Wait for 1 second
-    motor_pin.off()  # Turn the motor off
-    utime.sleep(1)  # Wait for 1 second
+    print_globals()
+    connect_to_wifi()
+    sleep(5)
+    api_get_boundary_parameters()
+    sleep(5)
+    print_globals()
